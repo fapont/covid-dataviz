e41aa105aedb5432dfb602dc1264883a21635c99
fapont <fa.pont@gmail.com>
2021-06-14 10:54:33 +0200

    Ebauche table

---
+++ web/static/js/table.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $('#table').DataTable();
+    console.log($.wi)
+} );
--- web/views.py
+++ web/views.py
@@ -11,8 +11,11 @@ COUNTRIES = []
 @app.route('/<page>')
 def showpage(page=None):
    if page:
+      if page == "table":
+         return showtable()
+      else:
       # called with page parameter
-      return render_template(f'{page}.html', countries=COUNTRIES)
+         return render_template(f'{page}.html', countries=COUNTRIES)
    else:
       # called with no parameters 
       return render_template('index.html')
@@ -22,12 +25,20 @@ def showpage(page=None):
 def send_data(path):
     return send_from_directory('data', path)
  
- 
- 
+def showtable():
+   data = pd.read_csv("web/data/covid-data.csv")
+   selection = ["continent", "location", "date", "total_cases", "new_cases", "total_deaths", "new_deaths"]
+   
+   data = data.iloc[:20,:] # test
+   return render_template('table.html', columns=selection, data=data[selection].values)
+
+
 @app.before_first_request
 def compute():
    # download last data here 
    data = pd.read_csv("web/data/covid-data.csv")
    global COUNTRIES
    COUNTRIES = data["location"].unique()
+   
+   
    
